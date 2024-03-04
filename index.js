dac8f2966ffb8d973bc3f610f1b4435f4324f76a
yuji314159 <yuji314159@gmail.com>
2024-03-05 00:41:02 +0900

    use @actions/core

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Hello, World!")
+const core = require("@actions/core");
+
+try {
+  const name = core.getInput("name");
+  const message = `Hello, ${name}!`;
+  console.log(message);
+  core.setOutput("message", message);
+} catch (error) {
+  core.setFailed(error.message);
+}
